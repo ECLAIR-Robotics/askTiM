ce2fac6680669e56f051edb7cdfebffcbb19f347
sarahwz <120886534+sarahwz@users.noreply.github.com>
2023-10-20 18:57:07 -0500

    chatgpt api

---
+++ chatgpt_api/generate_prompt.py
@@ -0,0 +1,28 @@
+import openai
+import config
+
+openai.api_key = config.open_ai_key
+
+def generate_prompt(text_sample):
+  completion = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    temperature = 0.8,
+    max_tokens = 2000,
+    messages = [
+      {"role": "system", "content": "You are an expert prompt generator for music generation machine learning models."},
+
+      {"role": "user", "content": "Generate a prompt using the template: 'a [mood] + [genre] song with [musical element]' given the following sample of text: \
+      Ah, distinctly I remember it was in the bleak December; \
+      And each separate dying ember wrought its ghost upon the floor. \
+      Eagerly I wished the morrow;—vainly I had sought to borrow \
+      From my books surcease of sorrow—sorrow for the lost Lenore— \
+      For the rare and radiant maiden whom the angels name Lenore— \
+      Nameless here for evermore."},
+      
+      {"role": "assistant", "content": "a depressing and spooky classical song with a cello melody"}, 
+      
+      {"role": "user", "content": f"Generate one based on this text sample: \
+      {text_sample}"}
+    ]
+  )
+  return completion.choices[0].message
